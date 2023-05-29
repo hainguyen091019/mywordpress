0af4eeb2079a76aa631f792bb6ae837c62a0a3a1
hainguyen091019 <134770893+hainguyen091019@users.noreply.github.com>
2023-05-29 13:30:48 +0700

    up lan 12

--- wp-login.php
+++ wp-login.php
@@ -4,11 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Đăng nhập &lsaquo;  &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
-<link rel="stylesheet" id="dashicons-css" href="https://github.com/hainguyen091019/mywordpress/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://github.com/hainguyen091019/mywordpress/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://github.com/hainguyen091019/mywordpress/wp-admin/css/forms.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://github.com/hainguyen091019/mywordpress/wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://github.com/hainguyen091019/mywordpress/wp-admin/css/login.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://hainguyen091019.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://hainguyen091019.github.io/mywordpress/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="forms-css" href="https://hainguyen091019.github.io/mywordpress/wp-admin/css/forms.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://hainguyen091019.github.io/mywordpress/wp-admin/css/l10n.min.css?ver=6.2.2" media="all">
+<link rel="stylesheet" id="login-css" href="https://hainguyen091019.github.io/mywordpress/wp-admin/css/login.min.css?ver=6.2.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -41,12 +41,12 @@
 		</form>
 
 					<p id="nav">
-				<a href="https://github.com/hainguyen091019/mywordpress/wp-login.php?action=lostpassword">Bạn quên mật khẩu?</a>			</p>
+				<a href="https://hainguyen091019.github.io/mywordpress/wp-login.php?action=lostpassword">Bạn quên mật khẩu?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="https://github.com/hainguyen091019/mywordpress/">&larr; Quay lại </a>		</p>
+			<a href="https://hainguyen091019.github.io/mywordpress/">&larr; Quay lại </a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -66,17 +66,17 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 
 					</form>
 				</div>
-				<script id="essential-blocks-blocks-localize-js-extra">var EssentialBlocksLocalize = {"eb_plugins_url":"https:\/\/github.com\/hainguyen091019\/mywordpress\/wp-content\/plugins\/essential-blocks\/","eb_wp_version":"6.2","eb_version":"4.0.8","eb_admin_url":"https:\/\/github.com\/hainguyen091019\/mywordpress\/wp-admin\/","rest_rootURL":"https:\/\/github.com\/hainguyen091019\/mywordpress\/wp-json\/","ajax_url":"https:\/\/github.com\/hainguyen091019\/mywordpress\/wp-admin\/admin-ajax.php","nft_nonce":"bb1135e6f3"};</script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-content/plugins/essential-blocks/assets/js/eb-blocks-localize.js?ver=4.0.8" id="essential-blocks-blocks-localize-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/github.com\/hainguyen091019\/mywordpress\/wp-includes\/js\/zxcvbn.min.js"};</script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+				<script id="essential-blocks-blocks-localize-js-extra">var EssentialBlocksLocalize = {"eb_plugins_url":"https:\/\/hainguyen091019.github.io\/mywordpress\/wp-content\/plugins\/essential-blocks\/","eb_wp_version":"6.2","eb_version":"4.0.8","eb_admin_url":"https:\/\/hainguyen091019.github.io\/mywordpress\/wp-admin\/","rest_rootURL":"https:\/\/hainguyen091019.github.io\/mywordpress\/wp-json\/","ajax_url":"https:\/\/hainguyen091019.github.io\/mywordpress\/wp-admin\/admin-ajax.php","nft_nonce":"bb1135e6f3"};</script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-content/plugins/essential-blocks/assets/js/eb-blocks-localize.js?ver=4.0.8" id="essential-blocks-blocks-localize-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.4" id="jquery-core-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/hainguyen091019.github.io\/mywordpress\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -86,17 +86,17 @@ wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/nguyenhai\/wp-admin\/admin-ajax.php"}};</script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"a73918f05f"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Hide":["Ẩn đi"],"Show":["Hiển thị"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."],"Hide password":["Ẩn mật khẩu"],"Show password":["Hiện mật khẩu"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
-<script src="https://github.com/hainguyen091019/mywordpress/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
+<script src="https://hainguyen091019.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 		<script>/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",(function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())}),!1);</script>
 			<div class="clear"></div>
 	</body>
